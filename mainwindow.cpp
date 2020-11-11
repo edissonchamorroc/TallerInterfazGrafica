1dd9d13f7c0f69a2be4b46e45666ed85eed75c4f
edisson chamorro <john.chamorro@udea.edu.co>
2020-11-11 18:57:35 -0500

    se agrega accion a todos los push botton

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,21 +18,21 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_suma_clicked()
 {
-    ui->resultado->setText(QString::number(ui->N1->value()+ui->N2->value()));
+    ui->resultado->setText(QString::number(ui->N1->value()+ui->N2->value()));//se realiza suma de numero
 
 }
 
 void MainWindow::on_resta_clicked()
 {
-
+    ui->resultado->setText(QString::number(ui->N1->value()-ui->N2->value()));//se realiza resta de numero
 }
 
 void MainWindow::on_mul_clicked()
 {
-
+    ui->resultado->setText(QString::number(ui->N1->value()*ui->N2->value()));//se realiza multi de numero
 }
 
 void MainWindow::on_div_clicked()
 {
-
+    ui->resultado->setText(QString::number(ui->N1->value()/ui->N2->value()));//se realiza div de numero
 }
